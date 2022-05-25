cdee882d5844311c345101edaa1b02cdec90d8d1
Anns Shahbaz <annsshahbaz@gmail.com>
2022-05-25 10:43:44 +0500

    Final Commit

---
+++ ViewComponents/ViewComponents/TaxSummary.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponents.ViewComponents
+{
+    public class TaxSummary : ViewComponent
+    {
+        public IViewComponentResult Invoke(int taxRate,int totalAmount)
+        {
+            ViewData["1"] = 1;
+            ViewData["2"] = "2";
+            ViewData["3"] = "3";
+            ViewData["TaxRate"] = taxRate;
+            ViewData["TotalAmount"] = totalAmount;
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/ViewComponents/WeatherSummary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ViewComponents.ViewComponents
+{
+    public class WeatherSummary : ViewComponent
+    {
+        public string Invoke()
+        {
+            string data = "this is weather informatoin";
+            return data;
+        }
+    }
+}
+
+//Bootstrap
+//Controller
+//Taghelpers
+//Seciton
+//RazorView
+//Layout
+//Partial Views
